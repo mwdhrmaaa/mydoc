af09b13dcdb3455d805320436fa684989aba049e
mwdhrmaaa <mwdhrmaaa@gmail.com>
2026-01-13 19:22:44 +0800

    feat: implement service worker for offline support

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'mydoc-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './src/style.css',
+  './src/main.js',
+  './src/storage.js',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
